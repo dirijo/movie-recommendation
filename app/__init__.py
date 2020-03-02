b31bf054123164f5a2049c35d6cc4395ac01e4ef
Renan <rvmoura.96@gmail.com>
2020-03-01 23:22:49 -0300

    Arquivos de configuração e inicialização da aplicação.

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from .models import configure as config_db
+from .serializer import configure as config_ma
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "sqlite:////Users/rvmoura/Documents/code/movie-recommendation/movies.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["DEBUG"] = True
+
+    config_db(app)
+    config_ma(app)
+    Migrate(app, app.db)
+
+    from .movies import bp_movies
+
+    app.register_blueprint(bp_movies)
+    return app
