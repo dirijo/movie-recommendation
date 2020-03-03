e8bc867722f9f44f3071dead3ea541e758626ea7
Renan Moura <renan.demoura96@dieboldnixdorf.com.br>
2020-03-03 14:22:57 -0300

    Refatoração das views/controller, inserção de novos testes e refatoração de testes antigos.

--- app/movies.py
+++ app/movies.py
@@ -1,24 +1,28 @@
-from flask import Blueprint, current_app, request, jsonify
+from flask import Blueprint, current_app, jsonify, redirect, request, url_for 
 from .models import Movie
 from .serializer import MovieSchema
 
 bp_movies = Blueprint("movies", __name__)
 
 
-@bp_movies.route("/exibir", methods=["GET"])
+@bp_movies.route("/list", methods=["GET"])
 def list():
     result = Movie.query.all()
     return MovieSchema(many=True).jsonify(result), 200
 
 
-@bp_movies.route("/detalhes/<int:movie_id>", methods=["GET"])
+@bp_movies.route("/details/<int:movie_id>", methods=["GET"])
 def detail(movie_id):
     ms = MovieSchema()
     result = Movie.query.filter(Movie.id == movie_id).first()
-    return ms.jsonify(result)
+    if result:
+        return ms.jsonify(result), 200
+    response = {
+        'message': f'404 - ID {movie_id} not found'
+    }
+    return jsonify(response), 404
 
-
-@bp_movies.route("/cadastrar", methods=["POST"])
+@bp_movies.route("/create", methods=["POST"])
 def create():
     ms = MovieSchema()
     movie = ms.load(request.json)
@@ -27,18 +31,23 @@ def create():
     return ms.jsonify(movie), 201
 
 
-@bp_movies.route("/deletar/<int:movie_id>", methods=["DELETE"])
+@bp_movies.route("/delete/<int:movie_id>", methods=["DELETE"])
 def delete(movie_id):
     remove_movie = Movie.query.filter(Movie.id == movie_id).delete()
     current_app.db.session.commit()
-    return f"ID {movie_id} deletado", 200
+    if remove_movie:
+        response = {"message": f"ID {movie_id} has been deleted"}
+        return jsonify(response), 200
+    response = {"message": f"404 - ID {movie_id}  not found"}
+    return jsonify(response), 404
 
 
-@bp_movies.route("/atualizar/<int:movie_id>", methods=["PUT"])
+@bp_movies.route("/update/<int:movie_id>", methods=["PUT"])
 def update(movie_id):
-    ms = MovieSchema()
-    movie_data = request.json
-    query = Movie.query.filter(Movie.id == movie_id)
-    query.update(movie_data)
+    query = Movie.query.filter(Movie.id == movie_id).update(request.json)
     current_app.db.session.commit()
-    return ms.jsonify(query.first()), 200
+    if query:
+        response = {'message': f'200 - ID {movie_id} updated'}
+        return jsonify(response), 200
+    response = {'message': f'404 - ID {movie_id} not found'}
+    return jsonify(response), 404
--- tests/test_system_movie.py
+++ tests/test_system_movie.py
@@ -29,11 +29,25 @@ class TestMovieBP(TestFlaskSystem):
             json=new_movie
         )
         expected = {
-            'id': 1,
-            'title': 'o violão triste'
+            'message': f'200 - ID {mv_object.id} updated'
         }
         self.assertEqual(expected, response.json)
         self.assertEqual(200, response.status_code)
+
+    def test_api_should_not_update_a_movie_with_wrong_id(self):
+        wrong_id = 23
+        new_movie = {
+            'title': 'o violão triste'
+        }
+        response = self.client.put(
+            url_for('movies.update', movie_id=wrong_id),
+            json=new_movie
+        )
+        expected = {
+            'message': f"404 - ID {wrong_id} not found"
+        }
+        self.assertEqual(expected, response.json)
+        self.assertEqual(404, response.status_code)
 
     def test_api_should_get_movie_detail(self):
         movie = {
@@ -50,6 +64,17 @@ class TestMovieBP(TestFlaskSystem):
         }
         self.assertEqual(expected, response.json)
         self.assertEqual(200, response.status_code)
+
+    def test_api_should_return_404_message_when_movie_does_not_exists(self):
+        wrong_id = 2
+        response = self.client.get(
+            url_for('movies.detail', movie_id=wrong_id)
+        )
+        expected = {
+            'message': f'404 - ID {wrong_id} not found'
+        }
+        self.assertEqual(expected, response.json)
+        self.assertEqual(404, response.status_code)
 
     def test_api_should_get_movie_list(self):
         first_movie = {'title': 'power rangers'}
@@ -79,6 +104,16 @@ class TestMovieBP(TestFlaskSystem):
         response = self.client.delete(
             url_for('movies.delete', movie_id=mv_object.id)
         )
-        expected = f'ID {mv_object.id} deletado'
-        self.assertEqual(expected, response.data.decode('ascii'))
+
+        expected = {"message": f"ID {mv_object.id} has been deleted"}
+        self.assertEqual(expected, response.json)
         self.assertEqual(200, response.status_code)
+
+    def test_api_should_return_404_when_id_does_not_exists(self):
+        wrong_id = 999999
+        response = self.client.delete(
+            url_for('movies.delete', movie_id=wrong_id)
+        )
+        expected = {'message': f"404 - ID {wrong_id}  not found"}
+        self.assertEqual(expected, response.json)
+        self.assertEqual(404, response.status_code)
